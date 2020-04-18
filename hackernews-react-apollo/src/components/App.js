cbe204cb9b8d383bccd0e98f761f7147487fc467
Peter Asenov <peter12345@abv.bg>
2020-04-18 16:27:10 +0300

    add Authentication

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -5,6 +5,7 @@ import '../styles/App.css';
 import Header from './Header';
 import CreateLink from './CreateLink';
 import LinkList from './LinkList';
+import Login from './Login';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -4,7 +4,9 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { setContext } from 'apollo-link-context';
 
+import { AUTH_TOKEN } from './constants';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
@@ -14,8 +16,18 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
