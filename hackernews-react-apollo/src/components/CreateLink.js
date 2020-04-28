8dc259da2a1c971a47b2938f1c318c9c628b04b9
Peter Asenov <peter12345@abv.bg>
2020-04-28 16:46:42 +0300

    add Pagination

--- hackernews-react-apollo/src/components/CreateLink.js
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
+import { LINKS_PER_PAGE } from '../constants';
 import { FEED_QUERY } from './LinkList';
 
 const POST_MUTATION = gql`
@@ -53,13 +54,20 @@ class CreateLink extends Component {
         <Mutation
           mutation={POST_MUTATION}
           variables={{ description, url }}
-          onCompleted={() => this.props.history.push('/')}
-          update={(store, { data: { postLink } }) => {
-            const data = store.readQuery({ query: FEED_QUERY })
-            data.feed.links.push(postLink)
+          onCompleted={() => this.props.history.push('/new/1')}
+          update={(store, { data: { post } }) => {
+            const first = LINKS_PER_PAGE
+            const skip = 0
+            const orderBy = 'createdAt_DESC'
+            const data = store.readQuery({
+              query: FEED_QUERY,
+              variables: { first, skip, orderBy }
+            })
+            data.feed.links.unshift(post)
             store.writeQuery({
               query: FEED_QUERY,
-              data
+              data,
+              variables: { first, skip, orderBy }
             })
           }}
         >
